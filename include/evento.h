6e3549c0929729ef632b6aa49153f8e619b3f5cf
amartinruiz23 <amr.antonio42@gmail.com>
2016-10-18 14:28:51 +0200

    Primer commit. Primeras funciones de evento

---
+++ include/evento.h
@@ -0,0 +1,23 @@
+#ifndef _EVENTO__
+#define _EVENTO__
+#include <string>
+
+class evento{
+  int anio;
+  string *sucesos;
+
+  public:
+
+  evento();
+
+  evento(evento e);
+
+  ~evento();
+
+  &evento operator=();
+
+  int getanio(evento e);
+
+  *string getsucesos(evento e); 
+
+};

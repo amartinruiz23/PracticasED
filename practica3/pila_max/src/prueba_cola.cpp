bbfc9c9ff66aba674a627177e52dbe38b1ce22c4
amartinruiz23 <amr.antonio42@gmail.com>
2016-11-10 18:33:45 +0100

    Reorganización. Arreglos en lista

---
+++ practica3/pila_max/src/prueba_cola.cpp
@@ -0,0 +1,27 @@
+#include "pila_max_cola.h"
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	Pila_max<int> mip ;
+
+	cout << "¿Vacia?" << mip.vacia() << endl ;
+
+	mip.poner(1);
+	mip.poner(2);
+	mip.poner(3);
+
+	cout << "¿Vacia?" << mip.vacia() << endl ;
+
+	cout << "Elemento tope" << mip.tope() << endl ;
+
+	mip.quitar();
+
+	cout << "Elemento tope" << mip.tope() << endl ;
+
+	Pila_max<int> mip2 = mip ;
+
+	cout << "Elemento tope" << mip2.tope() << endl ;
+
+}

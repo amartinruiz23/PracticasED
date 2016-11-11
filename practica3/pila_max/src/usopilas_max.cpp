104e6c090199a2d98f93b270aec13c8f87ebf7e9
amartinruiz23 <amr.antonio42@gmail.com>
2016-11-11 20:10:48 +0100

    uso corregido para nuestros clases

--- practica3/pila_max/src/usopilas_max.cpp
+++ practica3/pila_max/src/usopilas_max.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int main(){
 
-  Pila_max p;
+  Pila_max<int> p;
   int i;
 
-  for ( i=10; i>=0 ; i--) 
+  for ( i=10; i>=0 ; i--)
     p.poner(i);
-  
+
   while (!p.vacia() ){
-    elemento x = p.tope();
+    Par<int> x = p.tope();
     cout << x<<endl;
     p.quitar();
-  }     
-  
+  }
+
   return 0;
 }

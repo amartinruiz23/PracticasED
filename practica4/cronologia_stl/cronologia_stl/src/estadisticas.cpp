bdd7f98862ac1528dff99c1ae98590ec29b905e7
amartinruiz23 <amr.antonio42@gmail.com>
2016-12-11 21:10:50 +0100

    Compilado

--- practica4/cronologia_stl/cronologia_stl/src/estadisticas.cpp
+++ practica4/cronologia_stl/cronologia_stl/src/estadisticas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "cronologia.h"
 using namespace std;
 
@@ -17,17 +18,16 @@ int main (int argc, char* argv[]){
 		else {
 			Cronologia c ;
 			f >> c ;
-			int total_años=0 ;
-			int total_sucesos=0 ; 
+			int total_anios=0 ;
+			int total_sucesos=0 ;
 			int maximo=0 ;
-			int promedio=0 ;
+			double promedio=0 ;
 
-			c.estadisticas(total_años,total_sucesos,maximo,promedio);
-			cout << "Total de años para los que ha sucedido algún evento: " << total_años << endl ;
+			c.estadisticas(total_anios,total_sucesos,maximo,promedio);
+			cout << "Total de años para los que ha sucedido algún evento: " << total_anios << endl ;
 			cout << "Total de sucesos de la cronología: " << total_sucesos << endl ;
 			cout << "Máximo de eventos en un año: " << maximo << endl;
 			cout << "Promedio de eventos por año: " << promedio << endl;
 		}
 	}
 }
-

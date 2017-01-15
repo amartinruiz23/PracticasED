5b1a6b16b31e8128e3e57d7360d758505d0666ae
amartinruiz23 <amr.antonio42@gmail.com>
2017-01-15 19:03:45 +0100

    Constructor

--- practica_final/src/jugador_automatico.cpp
+++ practica_final/src/jugador_automatico.cpp
@@ -12,53 +12,36 @@ void GeneraHijos(nodo<Tablero>* n) {
 		if (n.etiqueta.hayHueco(i) != -1) {		// Si esa columna no está llena
 
 			Tablero t = n.etiqueta ;					// Copia el tablero
-
 			t.colocarFicha(i);								// Le insertas la ficha en la columna
 
 			if (contador == 0) {							// Le insertas el tablero al nodo como hijo a la izqda
-
 				n.izqda = t ;
 				contador++ ;
-
 			}
 
 			else {														// Le insertas el tablero al nodo como hijo a la derecha
-
-				ArbolGeneral<Tablero> a(t); 
-
+				ArbolGeneral<Tablero> a(t);
 				arbol.insertar_hermanoderecha(n , a );
-
 			}
+
 		}
 	}
 }
 
 conecta4(Tablero t) {
 
-	arbol.AsignaRaiz(t); // le asignamos la raíz al arbol que es el tablero que nos pasan
-	
-	nodo<Tablero> nodo_actual = arbol.raiz(); // creamos padre para trabajar con el
+	arbol.AsignaRaiz(t);
+	preorder_iterator(arbol.raiz()) it;
 
-	GeneraHijos(arbol.raiz()); // Genero hijos si es que se pueden generar
-		
-	int n_hijos = arbol.contar_hijos(padre) ;
-
-	while (altura(nodo_actual.izqda) < PROFUNDIDAD) {		// Mientras el nivel del primer hijo sea menor que el límite
-		
-		nodo_actual = nodo_actual.izqda ; 		// Bajamos un nivel, es decir, al primer hijo
-
-		for (int i = 1 ; i <= n_hijos ; i++) {
-
-			GeneraHijos(nodo_actual);
-
-		}
-
+	while (arbol.altura(*it) < PROFUNDIDAD) {
+		GeneraHijos(*it);
+		it++;
 	}
-		
 
 }
 
 int eligeColumna (int metrica){
+
   int columnaelegida;
   vector<int> puntacion;
   nodo<Tablero> señalado;
@@ -91,7 +74,3 @@ int metrica (int n){
 
 
 }
-
-
-
-

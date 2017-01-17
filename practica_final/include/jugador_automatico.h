e77028ac4c00873dbe92e823bc7943636b4828be
amartinruiz23 <amr.antonio42@gmail.com>
2017-01-17 14:36:46 +0100

    F rec

--- practica_final/include/jugador_automatico.h
+++ practica_final/include/jugador_automatico.h
@@ -6,26 +6,26 @@
 
 /**
    @brief T.D.A. conecta4
-   
+
    \b Definición:
    Una instancia \e c del tipo de dato abstracto conecta4 se se puede construir como
-   
+
    - Un árbol que contiene un elemento destacado, el nodo raíz, indispensable para
-	 crear el árbol, y dos valores, \e PROFUNDIDAD cuyo valor será decidido por los 
+	 crear el árbol, y dos valores, \e PROFUNDIDAD cuyo valor será decidido por los
 	 autores, y \e metrica valor suministrado por el usuario que indicará en cierta
    medida el grado de dificultad.
 
-	 En definitiva un objeto del tipo de dato abstracto conecta4 es el conjunto de 
+	 En definitiva un objeto del tipo de dato abstracto conecta4 es el conjunto de
    todos los movimientos posibles del jugador automático, que son almacenados en
 	 una estructura de tipo ArbolGeneral
-   
+
    Para poder usar el tipo de dato conecta4 se debe incluir el fichero
-   
+
    <tt>\#include conecta4.h</tt>
-   
+
    El espacio requerido para el almacenamiento es O(n), donde n es el número de
    nodos del árbol.
-   
+
    @authors Álvaro López Jiménez y Antonio Martín Ruíz.
    @date Enero de 2017
 */
@@ -43,10 +43,10 @@ class conecta4{
 	* @param n nodo padre de los futuros tableros a crear
 	*/
   void GeneraHijos(nodo<Tablero>* n);
-	
+
 	/**
 	* @brief Función que evalua la idoneidad o no de un tablero de cara a los intereses
-	* del jugador automático de cara a ganar la partida. 
+	* del jugador automático de cara a ganar la partida.
 	* @param t Tablero a evaluar
 	* @return entero con la puntuación obtenida
 	*/
@@ -69,10 +69,25 @@ public:
 	*/
 	~conecta4() = default ;
 
-  
+
 	int eligeColumna () ;
-	
+
 
 	int metrica (nodo<Tablero>* t);
-	
+
 };
+
+
+
+
+
+
+/*
+
+FUNCIÓN RECURSIVA
+
+CASO GENERAL: GENERA HIJOS DEL NODO Y LOS VALORA. RECIBE LAS VALORACIONES DE LOS HIJOS, SELECCIONA EL MEJOR, Y DEVUELVE LA VALORACIÓN Y EL HIJO QUE ELIGE.
+
+CASO BASE: VALORA EL NODO Y DEVUELVE -1 (NO TIENE HIJOS) Y SU VALORACIÓN. ESTE CASO SE DA CUANDO SE ESTÉ A LA PROFUNDIDAD MÁXIMA DEL ÁRBOL.
+
+*/
